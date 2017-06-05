30904c20f9ad3b67d681a0e50b1f38fd89d81864
downloadofvinh <downloadofvinh@gmail.com>
2017-06-05 13:35:28 +0700

    it have picture

--- app/src/main/java/com/example/healer/ieltsvocabulary/HomeActivity.java
+++ app/src/main/java/com/example/healer/ieltsvocabulary/HomeActivity.java
@@ -32,17 +32,5 @@ public class HomeActivity extends AppCompatActivity {
         GridView gv = (GridView) findViewById(R.id.listUnit);
         gv.setAdapter(homeAdapter);
     }
-    /*
-    public ArrayList<Unit> loadData(){
-        ArrayList<Unit> arrUnit = new ArrayList<Unit>();
-        Cursor c = myDataBase.rawQuery("SELECT * FROM UNIT", null);
-        c.moveToFirst();
-        String data = "";
-        while(c.isAfterLast() == false){
-            arrUnit.add(new Unit(c.getInt(0),c.getString(1).toString().trim(),Integer.parseInt(c.getString(2)),c.getString(3).trim().toString()));
-            c.moveToNext();
-        }
-        c.close();
-        return arrUnit;
-    }*/
+
 }
